fd4d10a0681101082feaf1a68c36e868448cfd5c
Anwar Bensouissi <Anwar777bensouissi@gmail.com>
2023-11-28 02:10:37 +0100

    Résolution des conflits de fusion

--- lib/view/profile.dart
+++ lib/view/profile.dart
@@ -26,8 +26,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   // Function to update user details
   Future<void> updateUser() async {
-    final Uri apiUrl =
-        Uri.parse('http://192.168.1.17:3000/api/v1/users/id/${widget.user.id}');
+    final Uri apiUrl =Uri.parse('http://192.168.38.65:3000/api/v1/users/id/${widget.user.id}');
 
     try {
       final http.Response response = await http.patch(
